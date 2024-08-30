3e9d3c8cd7a3ce179c48287894eb742eba1820fb
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-08-30 19:59:59 +0200

    Update on frontend part

--- teams/dumb_agent.py
+++ teams/dumb_agent.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class GomokuAgent:
     def __init__(self, agent_symbol, blank_symbol, opponent_symbol, max_depth=5):
-        self.name = "Tana's Agent"
+        self.name = "First Agent"
         self.agent_symbol = agent_symbol
         self.blank_symbol = blank_symbol
         self.opponent_symbol = opponent_symbol
